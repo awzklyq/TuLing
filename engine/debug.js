9a81406c68c5a2724ea130d58308e6dced51396f
awzklyq <awzklyq@163.com>
2017-02-15 23:17:31 +0800

    Add Debug.

---
+++ engine/debug.js
@@ -0,0 +1,35 @@
+function Debug( ) { }
+
+Debug.getFunctionName = function( func )
+{
+　　if (typeof func == 'function' || typeof func == 'object')
+	{
+		var name = ( '' + func ).match( /function\s*([\w\$]*)\s*\(/ );
+		return name && name[1];
+	}
+
+	return "";
+}
+
+if ( window.console == null )
+　　window.console = { };
+
+if ( console.trace == null )
+{
+	Debug.trace = function( )
+	{
+		var stack = [],
+		caller = arguments.callee.caller;
+		while (caller)
+		{
+			stack.unshift(getFunctionName(caller));
+			caller = caller && caller.caller;
+		}
+
+		log('functions on stack:' + '\n' + stack.join('\n'));
+	}
+}
+else
+{
+	Debug.trace = console.trace;
+}
